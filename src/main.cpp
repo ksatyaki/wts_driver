0162a3ba711e5571c7ae67f6d3073c21bd7aa2da
Chittaranjan Swaminathan Srinivas <chittaranjan.swaminathan@oru.se>
2016-07-15 15:28:08 +0200

    Add a parameter for frame rate

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,18 @@ int main(int argn, char* args[]) {
   wts_driver::SerialComm sc(args[1], 115200);
   wts_driver::WTSDriver wts_driver (sc);
 
+  float wts_frame_rate;
+
+  if(ros::param::get("/wts_frame_rate", wts_frame_rate)) {
+    ROS_INFO("Frame rate: %f", wts_frame_rate);
+  }
+  else {
+    wts_frame_rate = 20.0;
+    ROS_WARN("The \"wts_frame_rate\" parameter was not set. Setting frame rate to default value: 20 Hz.");
+  }
+
   wts_driver.initROSPublisher(nh);
-  wts_driver.startPeriodicFrameAcquisition(false, 50);
+  wts_driver.startPeriodicFrameAcquisition(false, 1000.0/wts_frame_rate);
 
   while(ros::ok()) {
     usleep(100000);
